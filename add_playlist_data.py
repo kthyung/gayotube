d1874bf13322954c64efb2af1073babe5a2994c5
TaeHyeong Kim <kthyung3@gmail.com>
2019-07-05 17:41:35 +0900

    no message

--- add_playlist_data.py
+++ add_playlist_data.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import django
 import time
+import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gayotube.settings")
 django.setup()
@@ -11,8 +12,8 @@ from video.models import Video, GenreVideo, PopVideo, PopGenreVideo, ArtistVideo
 from selenium import webdriver
 
 
-def parse_and_append(page_index, playlist_num):
-    print('kth parse_and_append() page_index : ' + str(page_index) + ' , playlist_num : ' + playlist_num)
+def parse_and_append():
+    print('kth parse_and_append()')
 
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
@@ -24,47 +25,71 @@ def parse_and_append(page_index, playlist_num):
     options.add_argument("lang=ko_KR")  # 한국어!
     driver = webdriver.Chrome('/Users/kimtaehyeong/Desktop/python/gayotube/chromedriver', options=options)
     driver.implicitly_wait(3)
-    driver.get('https://www.melon.com/mymusic/dj/mymusicdjplaylistview_inform.htm' + '?plylstSeq=' + playlist_num +
-               '&po=pageObj' + '&startIndex=' + page_index)
+    driver.get('https://www.melon.com/dj/themegenre/djthemegenre_list.htm')
     html = driver.page_source  ## 페이지의 elements모두 가져오기
     soup = BeautifulSoup(html, 'html.parser')  ## BeautifulSoup사용하기
 
-    titles = soup.find_all("div", {"class": "ellipsis rank01"})
-    artists = soup.find_all("div", {"class": "ellipsis rank02"})
+    recommend = soup.find("div", {"class": "recommend_list"})
 
-    title = []
-    artist = []
+    tag_code = []
 
-    if len(titles) == 0:
-        return
+    if recommend is not None:
+        a_thumbs = recommend.find_all("a", {"class": "image_typeAll"})
+        for a in a_thumbs:
+            if 'href' in a.attrs:
+                tag_script = a.attrs['href']
+                index = tag_script.find("goDjTagHub(")
+                tag_script2 = tag_script[index + 12:-3]
+                tag_code.append(tag_script2)
 
-    for t in titles:
-        if t.find('a') is None:
-            if t.find('div', {'class': 'ellipsis'}) is None:
-                title.append(t.find('span', {'class': 'checkEllipsis'}).text)
-            else:
-                title.append(t.find('div', {'class': 'ellipsis'}).text)
-        else:
-            title.append(t.find('a').text)
+    for i in range(len(tag_code)):
+        parse_and_append2(1, tag_code[i])
 
-    for a in artists:
-        if a.find('a') is None:
-            if a.find('div', {'class': 'ellipsis'}) is None:
-                artist.append(a.find('span', {'class': 'checkEllipsis'}).text)
-            else:
-                artist.append(a.find('div', {'class': 'ellipsis'}).text)
-        else:
-            artist.append(a.find('a').text)
+    driver.close()
+    driver.quit()
 
-    for i in range(len(title)):
-        videos = PopGenreVideo.objects.filter(type=genre_type, title=title[i]).order_by('chart')
-        if len(videos) == 0:
-            parse_and_append2(genre_type, page_index + i, title[i], artist[i])
-    parse_and_append(page_index + 50, playlist_num)
+def parse_and_append2(page_index, tag_num):
+    print('kth parse_and_append2() page_index : ' + str(page_index) + ' , tag_num : ' + tag_num)
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) "
+                         "AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/61.0.3163.100 Safari/537.36")
+    options.add_argument("lang=ko_KR")  # 한국어!
+    driver = webdriver.Chrome('/Users/kimtaehyeong/Desktop/python/gayotube/chromedriver', options=options)
+    driver.implicitly_wait(3)
+    driver.get('https://www.melon.com/dj/tag/djtaghub_list.htm' +
+               '?tagSeq=' + tag_num + '#params%5BtagSeq%5D=' + tag_num +
+               '&params%5BorderBy%5D=POP' + '&po=pageObj' + '&startIndex=' + page_index)
+    html = driver.page_source  ## 페이지의 elements모두 가져오기
+    soup = BeautifulSoup(html, 'html.parser')  ## BeautifulSoup사용하기
+
+    contents = soup.find_all("a", {"class": "image_typeAll"})
+
+    playlist_code = []
+
+    for c in contents:
+        if 'href' in c.attrs:
+            tag_script = c.attrs['href']
+            index = tag_script.find("goDjPlaylistDetail(")
+            tag_script2 = tag_script[index + 32:-3]
+            playlist_code.append(tag_script2)
 
 
-def parse_and_append2(video_type, chart, title, artist):
-    print('kth parse_and_append2() video_type : ' + str(video_type) + ' , chart : ' + str(chart)
+    for i in range(len(playlist_code)):
+        parse_and_append3(1, playlist_code[i])
+
+    driver.close()
+    driver.quit()
+
+    parse_and_append(page_index + 50, tag_num)
+
+
+def parse_and_append3(video_type, chart, title, artist):
+    print('kth parse_and_append3() video_type : ' + str(video_type) + ' , chart : ' + str(chart)
           + ' , title : ' + title + ' , artist : ' + artist)
 
     title2 = title.replace('&', ' N ', 1)
@@ -97,13 +122,13 @@ def parse_and_append2(video_type, chart, title, artist):
             break
 
     PopGenreVideo(type=video_type, chart=chart, title=title, artist=artist, video_key=key).save()
+
+    driver.close()
+    driver.quit()
     time.sleep(2)
 
 
 # 이 명령어는 이 파일이 import가 아닌 python에서 직접 실행할 경우에만 아래 코드가 동작하도록 합니다.
 if __name__=='__main__':
-    playlist = '447904040'
-    page = 1
-    parse_and_append(page, playlist)
-
-
+    parse_and_append()
+    sys.exit()
